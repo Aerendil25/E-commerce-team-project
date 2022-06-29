01440da01e40b3a9ce188a94ba8f25386001b0bd
MarsKG <mars.eshnazarov@gmail.com>
2022-06-29 19:46:25 -0400

    add-from-product-detail

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,8 +1,8 @@
 
 import Hero from "./Hero";
 import { useGlobal } from "../context/GlobalContext";
-export default function ProductDetails() {
-    const { selectedProduct } = useGlobal();
+export default function ProductDetails({product}) {
+    const { selectedProduct, handleAddToCart } = useGlobal();
 
     return (
 
@@ -14,7 +14,7 @@ export default function ProductDetails() {
                     <article className="single-product-info">
                         <div>
                             <h2 className="single-product-title">{selectedProduct.fields.name}</h2>
-                            <p className="single-product-company text-slanted">{selectedProduct.fields.name}</p>
+                            <p className="single-product-company text-slanted">by {selectedProduct.fields.company}</p>
                             <p className="single-product-price">${(selectedProduct.fields.price)/100}</p>
                             <div className="single-product-colors">
                                 {
@@ -30,7 +30,7 @@ export default function ProductDetails() {
                                 }
                             </div>
                             <p className="single-product-desc">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur earum doloribus architecto ipsum quasi! Ipsa suscipit provident alias a sunt itaque praesentium sed consequatur eius maxime quae excepturi quia perspiciatis, deleniti non. Aliquid obcaecati architecto error neque officia ex! Eum non, repudiandae quasi odio neque quam recusandae tempore suscipit incidunt.</p>
-                            <button className="addToCartBtn btn" data-id="id">add to cart</button>
+                            <button className="addToCartBtn btn" data-id="id" onClick={(e) => handleAddToCart(selectedProduct, e)}>add to cart</button>
                         </div>
                     </article>
                 </div> : "LOADING..."
