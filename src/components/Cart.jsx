1ca89b175fbee63600dd4a3ee2ce616bac885a69
MarsKG <mars.eshnazarov@gmail.com>
2022-06-29 16:35:57 -0400

    cart-item-inc/dec

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,12 @@
 import { useGlobal } from "../context/GlobalContext";
 export default function Cart() {
-  const { productsInCart, handleCloseCart } = useGlobal();
+  const {
+    productsInCart,
+    handleCloseCart,
+    uniqueProductsInCart,
+    handleAddToCart,
+    handleDeleteFromCart,
+  } = useGlobal();
   const itemPrice = productsInCart.map((item) => item.fields.price);
   const totalPrice = itemPrice.reduce((acc, item) => (acc += item), 0);
   console.log(totalPrice);
@@ -16,16 +22,42 @@ export default function Cart() {
             <h3 className="text-slanted">your items</h3>
           </header>
           <div className="cart-items">
-            {productsInCart.map((product) => (
-              <div>
-                <div>{product.fields.name}</div>
-                <div>${product.fields.price}</div>
-                <button>remove</button>
+            {uniqueProductsInCart.map((product) => (
+              <div key={product.id} className="cart-item">
+                <img
+                  className="cart-item-img"
+                  src={product.fields.image[0].url}
+                  alt="Not available"
+                />
+                <div>
+                  <div className="cart-item-name">{product.fields.name}</div>
+                  <div className="cart-item-price">
+                    ${product.fields.price / 100}
+                  </div>
+                  <button className="cart-item-remove-btn">remove</button>
+                </div>
+                <div>
+                  <button
+                    onClick={(e) => handleAddToCart(product, e)}
+                    className="cart-item-increase-btn"
+                  >
+                    <i className="fa-solid fa-chevron-up"></i>
+                  </button>
+                  <p className="cart-item-amount">
+                    {productsInCart.filter((p) => p.id === product.id).length}
+                  </p>
+                  <button
+                    onClick={(e) => handleDeleteFromCart(product, e)}
+                    className="cart-item-decrease-btn"
+                  >
+                    <i className="fa-solid fa-chevron-down"></i>
+                  </button>
+                </div>
               </div>
             ))}
           </div>
           <footer>
-            <h3 className="cart-total text-slanted">total : ${totalPrice}</h3>
+            <h3 className="cart-total text-slanted">total : ${totalPrice/100}</h3>
             <button className="cart-checkout btn">checkout</button>
           </footer>
         </aside>
